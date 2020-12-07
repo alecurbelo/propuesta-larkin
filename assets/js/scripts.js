b2b30adb5b458fb7dde57709bae8c10b296514e8
Adrian Antonio Garbey Fonseca <adrian.garbey@gmail.com>
2020-12-07 16:39:19 -0500

    Close

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,11 +1,14 @@
 (function ($) {
     $(function () {
-        $("#navbarSupportedContent a,.close-menu-icon").click(function(e){
-            alert('dsd');
+        $("#navbarSupportedContent a").click(function(e){
             $("#navbarSupportedContent").removeClass('show');
             $(".navbar-toggler").attr('aria-expanded', 'false');
         });
-
+        $(".close-menu-wrapper").click(function(e){
+            e.preventDefault();
+            $("#navbarSupportedContent").removeClass('show');
+            $(".navbar-toggler").attr('aria-expanded', 'false');
+        });
 
     });
 })(jQuery);
