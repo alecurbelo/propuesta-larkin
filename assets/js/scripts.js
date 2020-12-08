b92c300750eb15be652bf1f013e9554004f7a125
Adrian Antonio Garbey Fonseca <adrian.garbey@gmail.com>
2020-12-08 10:59:00 -0500

    Fijar loading.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,11 +1,10 @@
 (function ($) {
     $(function () {
-        $("#navbarSupportedContent a,.close-menu-wrapper").click(function(e){
+        $("#navbarSupportedContent a:not(.dropdown-toggle),.close-menu-wrapper").click(function(e){
             $("#navbarSupportedContent").removeClass('show');
             $(".navbar-toggler").attr('aria-expanded', 'false');
             $('.movil-over-color').fadeOut();
         });
-
 
         $(".navbar-toggler").click(function(){
             $('.movil-over-color').fadeIn();
