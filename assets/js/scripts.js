7e41b0cdd31a03a671ddcebdbf797feb15057347
Alejandro Curbelo <curbelo265@gmail.com>
2020-12-11 12:49:38 -0500

    read

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -14,10 +14,12 @@
 $(function(){
     $("#read-more").click(function(){
         $('.welcome-read-more-text').slideDown(2000);
+        $('#read-more').hide();
     });
     
     $("#view-less").click(function(){
-            $('.welcome-read-more-text').slideUp(2000);    
+            $('.welcome-read-more-text').slideUp(2000); 
+            $('#read-more').show();   
 
     });
     
