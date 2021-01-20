77bdac042c497930ffb4799ec4ff1f5a4e3963e4
Alejandro Curbelo <curbelo265@gmail.com>
2021-01-20 14:38:34 -0500

    form

--- assets/js/appointment.js
+++ assets/js/appointment.js
@@ -1,5 +1,6 @@
 var $firstButton = $(".first"),
   $secondButton = $(".second"),
+  $thirdButton = $ (".third"),
   $input = $("input"),
   $name = $(".name"),
   $more = $(".more"),
@@ -9,14 +10,14 @@ var $firstButton = $(".first"),
 
 $firstButton.on("click", function(e){
   $(this).text("Saving...").delay(900).queue(function(){
-    $ctr.addClass("center slider-two-active").removeClass("full slider-one-active");
+    $ctr.addClass("first slider-two-active").removeClass("slider-one-active");
   });
   e.preventDefault();
 });
 
 $secondButton.on("click", function(e){
   $(this).text("Saving...").delay(900).queue(function(){
-    $ctr.addClass("full slider-three-active").removeClass("center slider-two-active slider-one-active");
+    $ctr.addClass("center slider-three-active").removeClass("first slider-two-active");
     $name = $name.val();
     if($name == "") {
       $yourname.html("Anonymous!");
@@ -25,6 +26,13 @@ $secondButton.on("click", function(e){
   });
   e.preventDefault();
 });
+
+$thirdButton.on("click", function(e){
+    $(this).text("Saving...").delay(900).queue(function(){
+      $ctr.addClass("full slider-fourth-active").removeClass("center slider-three-active");
+    });
+    e.preventDefault();
+  });
 
 // copy
 //balapaCop("Step by Step Form", "#999");
